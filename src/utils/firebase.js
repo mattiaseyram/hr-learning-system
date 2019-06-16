1f56a8f3b8f446923f1f6b350d39c036d8aced4e
Mattias Eyram <mattiaseyram@gmail.com>
2019-06-16 19:36:08 -0400

    Added Travis CI

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,8 +2,11 @@ import config from './firebaseConfig';
 import firebase from 'firebase/app';
 import 'firebase/functions';
 
+const local = false;
 const app = firebase.initializeApp(config);
-//app.functions().useFunctionsEmulator('http://localhost:5000');
+
+if (local)
+    app.functions().useFunctionsEmulator('http://localhost:5000');
 
 export default app;
 export const functions = app.functions();
