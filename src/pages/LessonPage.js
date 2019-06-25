cc91dec35128980a2dd91062c5407112b103b346
Anton B <>
2019-06-25 16:21:40 -0400

    added form submit/change handlers to track user's answers

--- src/pages/LessonPage.js
+++ src/pages/LessonPage.js
@@ -17,6 +17,9 @@ export default function LessonPage({ match: { params } }) {
     const dispatch = useDispatch();
 
     const course = useSelector(getCourse) || {};
+    // storage of quiz answers
+    const quiz_ans = []
+
     const lesson = useSelector(getLesson) || {};
 
     console.log(course, lesson);
@@ -24,22 +27,38 @@ export default function LessonPage({ match: { params } }) {
     useEffect(() => { dispatch(fetchCourse(params.courseId)) }, [dispatch, params.courseId]);
     useEffect(() => { dispatch(fetchLesson(params.lessonId)) }, [dispatch, params.lessonId]);
 
+    // quiz form handler for submit
+    const handleSubmit = event => {
+        console.log(quiz_ans)
+        // TODO call backend function
+        event.preventDefault()
+    }
+
+    // handles input changes in the form
+    //  e.target.name == quiz #
+    //  e.target.id == value for answer in quiz #
+    const handleChange = e => {
+        quiz_ans[parseInt(e.target.name)] = e.target.id
+    }
+
     // quizzes
     const quizzes = (lesson.questions ? lesson.questions.map((quiz, i) => {
         return (
             <div>
                 <h4>{quiz.question}</h4>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group>
                         {quiz.options.map(ans => (
                             <Form.Check
                             type="radio"
                             label={ans}
-                            name={`quiz-group-${i}`}
+                            name={`${i}`}
                             id={ans}
+                            onChange={handleChange}
                         />
                         ))}
                     </Form.Group>
+                    <input type="submit" value="Submit" />
                 </Form>
             </div>
             )
