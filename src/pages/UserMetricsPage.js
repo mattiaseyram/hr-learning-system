e65793ed95c13d8da674f1b7b7a19b3db2f3f8be
prestidge.amanda <prestidge.amanda@gmail.com>
2019-06-27 23:02:12 -0400

    user metric view updated

--- src/pages/UserMetricsPage.js
+++ src/pages/UserMetricsPage.js
@@ -1,14 +1,14 @@
 //react
 import React, { useEffect } from 'react';
 //router
-import {Link } from "react-router-dom";
+import {} from "react-router-dom";
 //redux
 import { useDispatch, useSelector } from 'react-redux';
 import { getCourses, getLessons, getUser } from '../redux/selectors';
 import { fetchCourses, fetchLessons } from '../redux/actions';
 //react-bootstrap
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import ListGroup from 'react-bootstrap/ListGroup';
+
 //components
 import Page from '../components/Page';
 import JumboCard from '../components/JumboCard';
@@ -19,47 +19,76 @@ export default function UserMetricsPage() {
 
     const dispatch = useDispatch();
     
+    const user = useSelector(getUser);
     const courses = useSelector(getCourses);
     const lessons = useSelector(getLessons);
-    const user = useSelector(getUser);
+    var totals = 0;
+    var scores = 0;
 
     const name = user ? user.first_name : 'anonymous';
-
-    console.log({courses, lessons});
     
     //fetch user's courses and lessons when view loads
     useEffect(() => { dispatch(fetchCourses()) }, [dispatch]);
     useEffect(() => { dispatch(fetchLessons()) }, [dispatch]);
 
+    const average = Object.keys(courses).reduce((average, courseId) =>{
+        let complete = false;
+        try {
+            complete = user.courses[courseId].complete;
+        } catch (err) { }
+        if (complete){
+            const lessonAverage = Object.keys(lessons).reduce((lessonAverage, lessonId) => {
+                let lessonComplete=false;
+                try {
+                    if (user.courses[courseId].lessons[lessonId].complete){
+                        const lesson =user.courses[courseId].lessons[lessonId];
+                        return lessonAverage + (lesson.score / lesson.total);
+                    }
+                    else {
+                        return lessonAverage;
+                    }
+                } catch (err) {
+                    return lessonAverage;
+                 }
+        }, 0);
+        return average + (lessonAverage*100);
+    }
+    else{
+        return average;
+    }
+    },0);
+
     const courseItems = Object.keys(courses).map((courseId, i) => {
-
         const course = courses[courseId];
-        const scoreLessons = Object.keys(course.lessons).reduce((total, lessonId) => {
-            const lesson=course.lessons[lessonId];
-            console.log(lesson);
-            return total + lesson.score;
-        }, {score:0});
+        let complete = false;
+        try {
+            complete = user.courses[courseId].complete;
+        } catch (err) { }
 
-        const totalCourse =Object.keys(course.lessons).reduce((total, lessonId) => {
-            const lesson=course.lessons[lessonId];
-            return total + lesson.total;
-        }, {score:0});
-
-        const courseScore=scoreLessons / totalCourse;
-
-        return (
-            <ListGroup.Item>{course.title} {courseScore}
-            </ListGroup.Item>
-        );
+        if(complete){
+            const courseScore = Object.keys(lessons).reduce((sum, lessonId) => {
+                try {
+                    const lesson = user.courses[courseId].lessons[lessonId]
+                    return sum + ((lesson.score / lesson.total)*100);
+                } catch (err) {
+                    return sum;
+                 }
+            }, 0); 
+            return (
+                <ListGroup.Item>{course.title} - {courseScore}{"%"}
+                </ListGroup.Item>
+            );
+        }  
     });
 
     return (
         <Page>
             <JumboCard>
                 <h1>{name}{`'s Metrics`}</h1>
+                <body>{'Your average course score is '}{average}{'%!'}</body>
             </JumboCard>
             <ListGroup>
-                <ListGroup.Item className="list-group-header">Courses</ListGroup.Item>
+                <ListGroup.Item className="list-group-header">Course Scores</ListGroup.Item>
                 {courseItems}
             </ListGroup>
         </Page>
